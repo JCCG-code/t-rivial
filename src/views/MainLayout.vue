4b44db5fc5998e7cafc38dbd041122dcb3f89080
Juan Carlos Cuadrado Gracia <jccuadradogracia@gmail.com>
2023-01-25 17:58:07 +0100

    first-commit

---
+++ src/views/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main-layout">
+    <Header />
+    <Menu />
+    <AboutMe />
+    <Projects />
+    <Contact />
+  </div>
+</template>
+
+<script>
+import Header from "@/views/partials/HeaderPartial.vue";
+import Menu from "@/views/partials/MenuPartial.vue";
+import AboutMe from "@/views/partials/AboutMePartial.vue";
+import Projects from "@/views/partials/ProjectsPartial.vue";
+import Contact from "@/views/partials/ContactPartial.vue";
+
+export default {
+  name: "MainLayout",
+  components: {
+    Header,
+    Menu,
+    AboutMe,
+    Projects,
+    Contact
+  }
+};
+</script>
---
+++ src/views/partials/MenuPartial.vue
@@ -0,0 +1,5 @@
+<template>
+  <nav>
+    <h2>MENU</h2>
+  </nav>
+</template>
